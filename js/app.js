27da1462402f15e73cdbde2219270b4860c0c6c3
Jose Lucas <j.lucas.romualdo@gmail.com>
2025-08-28 15:09:57 -0300

    [05] - Atualização

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.testimonials-slider', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 1024px
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+    }
+});
